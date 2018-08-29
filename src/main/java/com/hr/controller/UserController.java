78c46d610ab7a4d6f9a4900d4dc6b5958ec6c654
hangjun <hangjun@hangjun-PC>
2018-08-29 14:16:54 +0800

    用户注册和登录功能实现

--- src/main/java/com/hr/controller/UserController.java
+++ src/main/java/com/hr/controller/UserController.java
@@ -7,35 +7,37 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hr.domain.User;
+import com.hr.dto.Result;
 import com.hr.enums.DbankErrorCode;
 import com.hr.service.UserService;
 import com.hr.utils.DES3Util;
 import com.hr.zException.DbankException;
 
-@Controller()
+@RestController()
 @RequestMapping("/user")
 public class UserController {
 	private Logger logger = LoggerFactory.getLogger(ExampleController.class);
-	@Autowired
-	private JdbcTemplate JdbcTemplate;
+//	@Autowired
+//	private JdbcTemplate JdbcTemplate;
 	@Autowired
 	private UserService userService;
 
 	@RequestMapping("/register")
-	public String register(User user) {
+	public Result register(User user) {
 		// check data
 		if (StringUtils.isEmpty(user.getUsername())) {
-			throw new DbankException(DbankErrorCode.UC000001);
+			return new Result<>(DbankErrorCode.UC000001);
 		}
 		// 判断用户名是否被使用
 		User queryUser = userService.queryByUsername(user.getUsername());
 		if (null != queryUser) {
-			throw new DbankException(DbankErrorCode.UC000002);
+			return new Result<>(DbankErrorCode.UC000002);
 		}
 		// 用户注册
 		String password = user.getPassword();
@@ -49,23 +51,23 @@ public class UserController {
 		user.setUpdateTime(new Date());
 		user.setCreateTime(new Date());
 		int i = userService.addUser(user);
-		if (i == 1) {
-			System.out.println("成功");
+		if (i != 1) {
+			return new Result<>(DbankErrorCode.UC999999);
 		}
-		return "success";
+		return new Result<>();
 	}
 
 	@RequestMapping("/login")
-	public String login(User user) throws UnsupportedEncodingException {
+	public Result login(User user,ModelAndView mv) throws UnsupportedEncodingException {
 		// check data
 		String username = user.getUsername();
 		if (StringUtils.isEmpty(username)) {
-			throw new DbankException(DbankErrorCode.UC000001);
+			return new Result<>(DbankErrorCode.UC000001); 
 		}
 		// 1.判断用户名是否存在
 		User userDb = userService.queryByUsername(user.getUsername());
 		if (null == userDb) {
-			throw new DbankException("","用户不存在");
+			return new Result<>(DbankErrorCode.UC000003);
 		}
 		String passwordDb=userDb.getPassword();
 		//2.请求密码3des加密,同时判断密码是否正确
@@ -73,9 +75,9 @@ public class UserController {
 		byte[] encrypt = DES3Util.encrypt(password, "123456781234567812345678");
 		password = new String(encrypt, "UTF-8");
 		if(!passwordDb.equals(password)) {
-			throw new DbankException("","密码不正确");
+			return new Result<>(DbankErrorCode.UC000004);
 		}
-		return "success";
+		return new Result<>();
 	}
 
 	@RequestMapping("/add")
--- src/main/java/com/hr/dto/Result.java
+++ src/main/java/com/hr/dto/Result.java
@@ -1,4 +1,8 @@
 package com.hr.dto;
+
+import com.hr.domain.User;
+import com.hr.enums.DbankErrorCode;
+
 /**
  * http请求返回的最外层对象
  * @author Administrator
@@ -7,40 +11,57 @@ package com.hr.dto;
 public class Result<T> {
 	/**
 	 * 错误码
+	 * 默认是000000
 	 */
-	private String code;
+	private String retCode="000000";
 	/**
 	 * 错误信息
+	 * 默认是交易成功
 	 */
-	private String msg;
+	private String retMsg="交易成功";
 	/**
 	 * 数据
 	 */
 	private T data;
-
-	public String getCode() {
-		return code;
+	
+	/**
+	 * 返回成功
+	 */
+	public Result() {
+		super();
 	}
-
-	public void setCode(String code) {
-		this.code = code;
+	/**
+	 * 返回成功+信息
+	 * @param data
+	 */
+	public Result(T data) {
+		this.data = data;
 	}
-
-	public String getMsg() {
-		return msg;
+	/**
+	 * 返回错误信息使用该方法
+	 * @param dbankErrorCode
+	 */
+	public Result(DbankErrorCode dbankErrorCode) {
+		this.retCode = dbankErrorCode.getCode();
+		this.retMsg = dbankErrorCode.getMsg();
 	}
-
-	public void setMsg(String msg) {
-		this.msg = msg;
+	public String getRetCode() {
+		return retCode;
 	}
-
+	public void setRetCode(String retCode) {
+		this.retCode = retCode;
+	}
+	public String getRetMsg() {
+		return retMsg;
+	}
+	public void setRetMsg(String retMsg) {
+		this.retMsg = retMsg;
+	}
 	public T getData() {
 		return data;
 	}
-
 	public void setData(T data) {
 		this.data = data;
 	}
 	
-
 }
