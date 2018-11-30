40ad6c223aa2b53784be925863331ece1b5725b3
hangjun <hangjun@hangjun-PC>
2018-11-30 10:48:59 +0800

    提交日志文件

--- src/main/java/com/hr/controller/IndexController.java
+++ src/main/java/com/hr/controller/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class IndexController {
 
-//	跳转用户登录页面
+//	跳转用户登录页面 
 	@RequestMapping("/login")
 	public String login() {
 		
--- src/main/java/com/hr/controller/UserController.java
+++ src/main/java/com/hr/controller/UserController.java
@@ -1,24 +1,22 @@
 package com.hr.controller;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Date;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.hr.domain.User;
 import com.hr.dto.Result;
 import com.hr.enums.DbankErrorCode;
 import com.hr.service.UserService;
-import com.hr.utils.DES3Util;
 
-@RestController()
+@Controller()
 @RequestMapping("/user")
 public class UserController {
 	private Logger logger = LoggerFactory.getLogger(ExampleController.class);
@@ -57,25 +55,28 @@ public class UserController {
 	}
 
 	@RequestMapping("/login")
-	public Result login(User user,ModelAndView mv) throws UnsupportedEncodingException {
+	public String login(User user,Model model) throws UnsupportedEncodingException {
 		// check data
 		String username = user.getUsername();
 		String password = user.getPassword();
 		if (StringUtils.isEmpty(username)) {
-			return new Result<>(DbankErrorCode.UC000001); 
+			model.addAttribute("msg", "用户名为空");
+			return "/login";
 		}
 		if (StringUtils.isEmpty(password)) {
-			return new Result<>(DbankErrorCode.UC000001); 
+			model.addAttribute("msg", "用户名为空");
+			return "/login"; 
 		}
 		//根据用户名和密码查询用户;如果email也不为空,三个条件一起查
 		List<User> userList= userService.queryByUsernameAndPassword(user);
 		if(null==userList || userList.size()!=1) {
-			return new Result<>(DbankErrorCode.UC000002); 
+			model.addAttribute("msg", "用户名不存在");
+			return "/login";  
 		}
 		
 //		byte[] encrypt = DES3Util.encrypt(password, "123456781234567812345678");
 //		password = new String(encrypt, "UTF-8");
-		return new Result<>();
+		return "user/main";
 	}
 
 	
